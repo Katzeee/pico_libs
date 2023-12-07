4505df942eb0f458ee5cb939d77852812985cb03
xac <jiangfengxi.c@gmail.com>
2023-12-07 23:58:08 +0800

    [feat]:mpl first version of typelist

--- mpl/include/type_list.hpp
+++ mpl/include/type_list.hpp
@@ -1,1 +1,51 @@
-namespace xac::mpl {}
+#pragma once
+#include <cstdint>
+#include <utility>
+
+namespace xac::mpl {
+
+template <typename... Args>
+struct type_list {
+  constexpr static uint32_t size = sizeof...(Args);
+};
+
+template <uint32_t N, typename T>  // T can be any type, include a template type
+struct nth_type;
+
+// specialization for template class T
+// here Head and Rest are prepared for T
+// T must be a template type itself rather than a specialization of template type
+// ...Args here are just for deducing the T and what are Args
+// so we must use specialization to implement this
+// without the raw template, we cannot do type deducing
+template <template <typename...> class T, typename Head, typename... Rest>
+struct nth_type<0, T<Head, Rest...>> : std::common_type<Head> {};
+
+template <uint32_t N, template <typename...> class T, typename Head, typename... Rest>
+struct nth_type<N, T<Head, Rest...>> : nth_type<N - 1, T<Rest...>> {
+  static_assert(N < sizeof...(Rest) + 1, "123");
+};
+
+template <template <typename...> class U, typename T>
+struct rename {};
+template <template <typename...> class U, template <typename...> class T, typename... Args>
+struct rename<U, T<Args...>> {
+  using type = U<Args...>;
+};
+
+struct type_id {
+  inline static uint32_t counter = 0;
+  template <typename T>
+  inline static uint32_t value = counter++;
+};
+
+template <typename T, typename TList>
+struct is_in_type_list : std::false_type {};
+
+template <typename T, template <typename...> class TList, typename... Rest>
+struct is_in_type_list<T, TList<T, Rest...>> : std::true_type {};
+
+template <typename T, template <typename...> class TList, typename Head, typename... Rest>
+struct is_in_type_list<T, TList<Head, Rest...>> : is_in_type_list<T, TList<Rest...>> {};
+
+}  // namespace xac::mpl
--- tests/mpl_test.cpp
+++ tests/mpl_test.cpp
@@ -1,5 +1,13 @@
+#include <type_list.hpp>
+using namespace xac::mpl;
+
 #include "gtest/gtest.h"
 
 TEST(MPL_TEST, TYPE_LIST) {
-
+  const bool v = is_in_type_list<bool, type_list<bool, int>>::value;
+  const bool v1 = is_in_type_list<bool, type_list<int>>::value;
+  const bool v2 = is_in_type_list<bool, type_list<int, bool>>::value;
+  EXPECT_EQ(v, true);
+  EXPECT_EQ(v1, false);
+  EXPECT_EQ(v2, true);
 }
