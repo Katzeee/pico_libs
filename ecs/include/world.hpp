b1a641ea6f57c81311b31a0bb02c4141e973bfea
xac <jiangfengxi.c@gmail.com>
2023-12-08 20:00:36 +0800

    [feat]:ecs add view and iterator

--- ecs/include/world.hpp
+++ ecs/include/world.hpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <functional>
 #include <memory>
+#include <numeric>
 #include <type_list.hpp>
 #include <vector>
 
@@ -25,6 +26,73 @@ class World {
   using Entity = Entity<TSettings>;
   using EntityId = typename Entity::Id;
 
+  template <typename...>
+  class component_view;
+
+  template <typename... Args>
+  class component_iterator {  // HINT: after c++17, std::iterator is deperated
+   public:
+    using value_type = typename component_view<Args...>::value_type;
+    using type = component_iterator<Args...>;
+    component_iterator(World *world, uint32_t i) : world_(world), i_(i) {
+      next();
+    }
+    auto operator*() -> value_type {
+      return {std::get<mpl::index_of_v<std::decay_t<Args>, ComponentList>>(world_->components_pool_).at(i_)...};
+    }
+    auto operator++(int) -> type {
+      auto temp = this;
+      ++(*temp);
+      return *temp;
+    }
+    auto operator++() -> type & {
+      i_++;
+      next();
+      return *this;
+    }
+    friend auto operator==(const component_iterator<Args...> &lhs, const component_iterator<Args...> &rhs) -> bool {
+      assert(lhs.world_ == rhs.world_);
+      return lhs.i_ == rhs.i_;
+    }
+
+   private:
+    auto next() -> void {
+      for (; i_ < entity_count_; i_++) {
+        if ((component_view<Args...>::mask_ & world_->entities_.at(i_).components_mask_) ==
+            component_view<Args...>::mask_) {
+          break;
+        }
+      }
+    }
+
+   private:
+    World<TSettings> *world_;
+    uint32_t i_;
+  };
+
+  template <typename... Args>
+  class component_view {
+    friend class component_iterator<Args...>;
+
+   public:
+    using value_type = std::tuple<std::remove_reference_t<Args> &...>;
+    // using value_type = std::tuple<Args &&...>;
+    component_view(World<TSettings> *world) : world_(world) {}
+    auto begin() -> component_iterator<Args...> {
+      return component_iterator<Args...>{world_, 0};
+    }
+    auto end() -> component_iterator<Args...> {
+      return component_iterator<Args...>(world_, entity_count_);
+    }
+
+   private:
+   private:
+    World<TSettings> *world_;
+    inline constexpr static uint32_t mask_value_ =
+        mpl::index_sequence_v<mpl::index_of_v<std::decay_t<Args>, ComponentList>...>;
+    inline static std::bitset<ComponentList::size> mask_ = std::bitset<ComponentList::size>(mask_value_);
+  };
+
  public:
   World();
   auto create() -> EntityId;
@@ -32,11 +100,14 @@ class World {
   auto assign(EntityId &id, Args &&...args) -> std::shared_ptr<ComponentHandle>;
   template <typename F>
   auto each(F &&f);
+  template <typename... Args>
+  auto view() -> component_view<Args...>;
 
  private:
   auto prepare_entity_create() -> void;
   template <typename T>
   auto prepare_component_create(const EntityId &id) -> void;
+  // generate view mask in compile time
 
  private:
   mpl::rename<TupleOfVectors, ComponentList> components_pool_;
@@ -52,9 +123,6 @@ template <typename TSettings>
 World<TSettings>::World() {
   entities_.resize(kInitSize);
   entity_version_.resize(kInitSize);
-  // for (uint32_t i = 0; i < ComponentList::size; i++) {
-  //   std::get<0>(components_pool_).resize(kInitSize);
-  // }
 }
 
 // TODO: reuse id
@@ -63,8 +131,8 @@ auto World<TSettings>::create() -> EntityId {
   prepare_entity_create();
   auto id = EntityId{};
   id.id = entity_count_;
-  id.version = ++entity_version_[entity_count_];
-  entities_[entity_count_] = std::move(Entity{id, this});
+  id.version = ++entity_version_.at(entity_count_);
+  entities_.at(entity_count_) = std::move(Entity{id, this});
   entity_count_++;
   return id;
 }
@@ -74,8 +142,8 @@ template <typename T, typename... Args>
 auto World<TSettings>::assign(EntityId &id, Args &&...args) -> std::shared_ptr<ComponentHandle> {
   static_assert(TSettings::template has_component<T>(), "type is not in component list");
   prepare_component_create<T>(id);
-  auto &entity = entities_[id.id];
-  auto &real_version = entity_version_[id.id];
+  auto &entity = entities_.at(id.id);
+  auto &real_version = entity_version_.at(id.id);
   real_version++;
   id.version = real_version;
   entity.id_ = id;
@@ -91,6 +159,12 @@ auto World<TSettings>::each(F &&f) {
   for (uint32_t i = 0; i < entity_count_; i++) {
     std::invoke(f, entities_[i], i);
   }
+}
+
+template <typename TSettings>
+template <typename... Args>
+auto World<TSettings>::view() -> component_view<Args...> {
+  return component_view<Args...>{this};
 }
 
 template <typename TSettings>
@@ -112,7 +186,8 @@ auto World<TSettings>::prepare_entity_create() -> void {
   assert(entities_.size() == entity_version_.size());
   if (entity_count_ >= entities_.size()) {
     // FIX: may out of bound
-    entities_.resize(entity_count_ * 2);  // HINT: must be resize, because you will use index
+    assert(entity_count_ * 2 <= std::numeric_limits<uint32_t>::max());
+    entities_.resize(entity_count_ * 2, {});  // HINT: must be resize, because you will use index
     entity_version_.resize(entity_count_ * 2);
   }
 }
--- tests/ecs_test.cpp
+++ tests/ecs_test.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
+#include <random>
 #include <world.hpp>
-using namespace xac;
 
 #include "gtest/gtest.h"
+using namespace xac;
 
 struct Position {
   int x;
@@ -24,32 +26,84 @@ struct Rotation {
 using CList = mpl::type_list<Position, Acc, Rotation>;
 using Settings = ecs::Settings<CList>;
 
-static ecs::World<Settings> world;
-
 TEST(ECS_TEST, ENTITY_CREATE) {
+  uint16_t entity_count = std::numeric_limits<uint16_t>::max();
+  ecs::World<Settings> world;
   std::vector<ecs::Entity<Settings>::Id> entities;
-  for (auto i = 0; i < 50000; i++) {
+  for (uint16_t i = 0; i < entity_count; i++) {
     auto e = world.create();
     entities.push_back(e);
-    EXPECT_EQ(e.id, i);
+    ASSERT_EQ(e.id, i);
     EXPECT_EQ(e.version, 1);
   }
-  world.each([](auto &&e, uint32_t i) {
+}
+
+TEST(ECS_TEST, COMPONENT_ASSIGN) {
+  // FIXME: Should use another setting
+  ecs::World<Settings> world;
+  std::random_device rd;
+  std::mt19937 seed(rd());
+  std::uniform_int_distribution<uint32_t> rand_int(500, 500000);
+  uint32_t entity_count = rand_int(seed);
+  std::cout << entity_count << std::endl;
+  std::vector<ecs::Entity<Settings>::Id> entities;
+  for (auto i = 0; i < entity_count; i++) {
+    auto e = world.create();
+    entities.push_back(e);
+    ASSERT_EQ(e.id, i);
+    EXPECT_EQ(e.version, 1);
+  }
+  world.each([&world](auto &&e, uint32_t i) {
     EXPECT_EQ(e.GetId().id, i);
     EXPECT_EQ(e.GetId().version, 1);
     EXPECT_EQ(e.GetWorld(), &world);
     EXPECT_EQ(e.GetComponentsMask().to_string(), "000");
   });
-  for (auto i = 0; i < 50000; i++) {
+  for (auto i = 0; i < entity_count; i++) {
     auto &e = entities[i];
     world.assign<Position>(e, 1, 2, 3);
     EXPECT_EQ(e.id, i);
     EXPECT_EQ(e.version, 2);
   }
-  world.each([](auto &&e, uint32_t i) {
+  world.each([&world](auto &&e, uint32_t i) {
     EXPECT_EQ(e.GetId().id, i);
     EXPECT_EQ(e.GetId().version, 2);
     EXPECT_EQ(e.GetWorld(), &world);
     EXPECT_EQ(e.GetComponentsMask().to_string(), "001");
   });
+  {
+    auto view = world.view<const Position>();
+    auto it = view.begin();
+    static_assert(std::is_same_v<decltype(*it), std::tuple<const Position &>>);
+    int count = 0;
+    for (; it != view.end(); it++) {
+      count++;
+      auto &&[position] = *it;
+      EXPECT_EQ(position.x, 1);
+      EXPECT_EQ(position.y, 2);
+      EXPECT_EQ(position.z, 3);
+    }
+    EXPECT_EQ(count, entity_count);
+  }
+  {
+    auto view = world.view<Position>();
+    auto it = view.begin();
+    static_assert(std::is_same_v<decltype(*it), std::tuple<Position &>>);
+  }
+  {
+    auto view = world.view<Rotation, Position>();
+    auto it = view.begin();
+    static_assert(std::is_same_v<decltype(*it), std::tuple<Rotation &, Position &>>);
+    EXPECT_EQ(it, view.end());
+  }
+  {
+    auto view = world.view<const Position, Acc>();
+    auto it = view.begin();
+    static_assert(std::is_same_v<decltype(*it), std::tuple<const Position &, Acc &>>);
+  }
+  {
+    auto view = world.view<Acc &&, Rotation &>();
+    auto it = view.begin();
+    static_assert(std::is_same_v<decltype(*it), std::tuple<Acc &, Rotation &>>);
+  }
 }
